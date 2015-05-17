a355556ce7f99237053614a70b3e983f3016430c
Haukur Kristinsson <haukur@hauxi.is>
2015-05-17 17:27:31 +0000

    tests for socket module ready

--- tests/test_socket.py
+++ tests/test_socket.py
@@ -1,3 +1,6 @@
+from qlivestats.core.qsocket import Socket, SocketConnectionError
+from qlivestats.core import config
+
 from unittest import TestCase
 
 from tests import tests_dir
@@ -7,11 +10,15 @@ class SocketTests(TestCase):
 
     def __init__(self, *args, **kwargs):
         super(BasicConfigTest, self).__init__(*args, **kwargs)
+        self.cfg = config.YAMLConfig(os.path.join(tests_dir, 'configtst/qlivestats.yaml'))
 
-    def test_read_socket(self):
-        # hehe
+    def test_connect_socket(self):
+        _socket = qsocket.Socket(self.cfg["livestatus"]["broker"])
+        self.assertEqual(_socket.connect(),True)
 
-
+    def test_failconnect_socket(self):
+        _socket = qsocket.Socket("/var/lib/share/somesocketthatdoesnotexist")
+        self.assertRaises(SocketConnectionError, lambda: list(_socket.connect()))
 
 
 
