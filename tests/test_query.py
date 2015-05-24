eef33c86edeb2f8beb809f96f2fa22faf150a442
Haukur Kristinsson <haukur@hauxi.is>
2015-05-24 16:19:30 +0000

    JSON changes trying to fix buggy output from LiveStatus

--- tests/test_query.py
+++ tests/test_query.py
@@ -1,5 +1,5 @@
 from qlivestats.core import config
-from qlivestats.core.query import Query, NotSupportTableError, BrokerNotSpecified
+from qlivestats.core.query import Query, NotSupportTableError, BrokerNotSpecified, NoTableSpecifiedError, JSONNotWellFormedFromServer 
 from qlivestats.core.qsocket import Socket
 
 from unittest import TestCase
@@ -16,13 +16,17 @@ class QueryTests(TestCase):
         self._q = Query(self.cfg.get_broker())
             
     def test_query_hosts_with_no_filters(self):
-        self.assertEqual(str(self._q.hosts), "GET hosts\nOutputFormat: python\n")
+        self.assertEqual(str(self._q.hosts), "GET hosts\nOutputFormat: json\nColumnHeaders: on\n")
+
+    def test_query_hosts_with_reset_filters(self):
+        self.assertEqual(str(self._q.hosts.Columns("host name")),"GET hosts\nColumns: host name\nOutputFormat: json\nColumnHeaders: on\n")
+        self.assertEqual(str(self._q.hosts), "GET hosts\nOutputFormat: json\nColumnHeaders: on\n")
 
     def test_query_services_with_no_filters(self):
-        self.assertEqual(str(self._q.services), "GET services\nOutputFormat: python\n")
+        self.assertEqual(str(self._q.services), "GET services\nOutputFormat: json\nColumnHeaders: on\n")
 
     def test_query_logs(self):
-        self.assertEqual(str(self._q.log), "GET log\nOutputFormat: python\n")
+        self.assertEqual(str(self._q.log), "GET log\nOutputFormat: json\nColumnHeaders: on\n")
 
     def test_query_hosts_specific_columns(self):
         self._q.hosts.Column("host_name") \
@@ -30,12 +34,12 @@ class QueryTests(TestCase):
         .Column("state")
 
         self.assertEqual(str(self._q), "GET hosts\nColumns: host_name "
-                         "description state\nOutputFormat: python\n")
+                         "description state\nOutputFormat: json\nColumnHeaders: on\n")
 
     def test_query_services_filter(self):
         query = self._q.services.Filter("state = 2")
 
-        self.assertEqual(str(query), "GET services\nFilter: state = 2\nOutputFormat: python\n")
+        self.assertEqual(str(query), "GET services\nFilter: state = 2\nOutputFormat: json\nColumnHeaders: on\n")
 
     def test_query_services_multiple_filters(self):
         query = self._q.services \
@@ -43,7 +47,7 @@ class QueryTests(TestCase):
         .Filter("in_notification_period = 1")
 
         self.assertEqual(str(query), "GET services\nFilter: state = 2\n"
-                         "Filter: in_notification_period = 1\nOutputFormat: python\n")
+                         "Filter: in_notification_period = 1\nOutputFormat: json\nColumnHeaders: on\n")
 
     def test_bad_table(self):
         self.assertRaises(NotSupportTableError, lambda: list(self._q.somebadtable))
@@ -58,12 +62,18 @@ class QueryTests(TestCase):
     def test_bad_config_when_getting_default(self):
         self.assertRaises(config.ConfigReadError, lambda: list(Query()))
 
-    def test_run_query(self):
+    def test_run_query_parse_json(self):
         with mock.patch('qlivestats.core.qsocket.socket.socket') as socketmock:
-            socketmock.return_value.recv.return_value = "1+2"
+            socketmock.return_value.recv.return_value = '[["host_name"], ["server01"]]'
             socketmock.return_value.connect.return_value = ''
             res = self._q.services.run()
-            self.assertEqual(res, 3)
+            self.assertEqual(res, [{"host_name": "server01"}])
 
+    def test_run_query_parse_bad_json(self):
+        with mock.patch('qlivestats.core.qsocket.socket.socket') as socketmock:
+            socketmock.return_value.recv.return_value = '["foo", {"bar}]'
+            socketmock.return_value.connect.return_value = ''
+            self.assertRaises(JSONNotWellFormedFromServer, lambda: list(self._q.services.run()))
 
-
+    def tests_query_on_no_table(self):
+        self.assertRaises(NoTableSpecifiedError, lambda: list(self._q.run()))
