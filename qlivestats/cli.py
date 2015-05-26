54c1f8064839c33a6814dc9930c65acd8b324195
Haukur Kristinsson <haukur@hauxi.is>
2015-05-26 10:32:04 +0000

    modification for better cli experience

--- qlivestats/cli.py
+++ qlivestats/cli.py
@@ -1,7 +1,15 @@
 import click
+import pprint
+from tabulate import tabulate
 from qlivestats.core.query import Query
 from qlivestats.core import config as configmod
 
+def getSupportedTables():
+    return ["hosts","services","hostgroups","servicegroups",
+            "contactgroups","servicesbygroup","servicesbyhostgroup",
+            "hostsbygroup","contacts","commands","timeperiods",
+            "downtimes","comments","log","status","columns",
+            "statehist"]
 
 @click.command()
 @click.option('--config', default="/etc/qlivestats.yaml", help='Config File Location')
@@ -22,18 +30,25 @@ def qlivestatsquery(config, table, filters, columns):
     query = getattr(query, table)
 
     # Construct the query.
-    
+    if table not in getSupportedTables():
+        raise click.BadParameter('You need to specify a table that is supported by Live Status.'
+                                 '', param_hint=['--table']) 
+
     if filters:
         for filter in filters.split(","):
             query = query.Filter(filter)
     
     if columns:
         cols = columns.replace(","," ")
-        query = query.Columns(cols)
-
+        query = query.Columns(cols) 
+    else:
+        click.pause(info="\n[INFO] Selecting all columns from tables can create unreadable output."
+                    "\n       It's recommeded to select the columns you are interested in."
+                    "\n       You can see possible columns by using qlivestat-describe [table]"
+                    "\n       [ Press any key if you want to continue! ]", err=False)
     res = query.run()
 
-    click.echo(res)
+    click.echo(tabulate(res, tablefmt="rst", numalign="right", headers="keys"))
 
 @click.command()
 @click.option('--config', default="/etc/qlivestats.yaml", help='Config File Location')
@@ -41,6 +56,11 @@ def qlivestatsquery(config, table, filters, columns):
                             help='Table can include hosts, services, contacts etc.')
 def qlivestatsdescribe(config, table):
     """ Describe a table in Live Status broker"""
+
+    # Construct the query.
+    if table not in getSupportedTables():
+        raise click.BadParameter('You need to specify a table that is supported by Live Status.'
+                                 '', param_hint=['--table']) 
 
     # Create a Query object
     cfg = configmod.YAMLConfig(config)
@@ -50,4 +70,4 @@ def qlivestatsdescribe(config, table):
 
     res = query.Describe()
 
-    click.echo(res)
+    click.echo(pprint.pprint(res))
--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ setup(
         '': ['*.yaml']
     },
     include_package_data = True,
-    install_requires=['Click'],
+    install_requires=['Click==4.0.0','pyyaml','simplejson'],
     url='https://github.com/haukurk/qlivestats',
-    download_url='https://github.com/haukurk/qlivestats/releases/tag/v0.1.0',
+    download_url='https://github.com/haukurk/qlivestats/releases/tag/v0.1.1',
     keywords=['nagios', 'livestats'],
-    test_requires=['mock'],
+    tests_require=['pytest', 'tox', 'mock', 'coverage', 'coveralls'],
     classifiers=[],
     entry_points={
         'console_scripts': [
